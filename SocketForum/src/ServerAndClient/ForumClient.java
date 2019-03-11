e90cc07e8d7e0f7bc6eeb1ce47b5f0518a4db4e4
Absalom Herrera <45426790+elpatsan@users.noreply.github.com>
2019-03-11 12:44:57 -0600

    headers deletion, client class creation

---
+++ SocketForum/src/ServerAndClient/ForumClient.java
@@ -0,0 +1,22 @@
+package ServerAndClient;
+
+import DTO.Post;
+import java.net.Socket;
+
+/**
+ *
+ * @author Absalom Herrera
+ */
+public class ForumClient {
+    
+    private Socket so;
+    private int port;
+    private String host;
+    
+    public ForumClient() {
+    }
+    
+    public void createPost (Post p){
+        
+    }
+}
