cc96899f023940481d29fe83b25bea67e785d45f
Juan Carlos <carlos_isc7@outlook.com>
2019-03-28 12:49:31 -0600

    deleted ghost GUI

--- SocketForum/src/GUIs/ClientMainGUI.java
+++ SocketForum/src/GUIs/ClientMainGUI.java
@@ -24,9 +24,8 @@ public class ClientMainGUI extends JFrame implements ActionListener {
 
     private static JButton botonDe1, botonDe2, searchButton;
     private static JTextField searchTextField;
-    private JButton btnComments[];
-    BufferedImage myPicture;
-    JLabel picLabel;
+    private BufferedImage myPicture;
+    private JLabel picLabel;
 
     private String user;
     private ForumClient fc;
@@ -35,18 +34,31 @@ public class ClientMainGUI extends JFrame implements ActionListener {
 
     private List allPosts = null;
     private int total_btns;
-    private List comments[];
+    private ArrayList<List> comments;
     private List posts;
     private String current_user;
-
+    JPanel ssp1, ssp2;
+    
+    private ArrayList<JLabel> titlesLabels;
+    private ArrayList<JLabel> topicsLabels;
+    private ArrayList<JLabel> usersLabel;
+    private ArrayList<JLabel> messagesLabel;
+    private ArrayList<JLabel> picsLabel;
+    private ArrayList <JButton> buttonsComment;
     public ClientMainGUI(String user) {
         current_user = user;
-        JFrame frame = new JFrame();
+        //JFrame this = new JFrame();
         postWithImage = new ArrayList<>();
+        titlesLabels = new ArrayList<>();
+        messagesLabel = new ArrayList<>();
+        topicsLabels = new ArrayList<>();
+        usersLabel = new ArrayList<>();
+        picsLabel = new ArrayList<>();
+        buttonsComment = new ArrayList<>();
         total_btns = 0;
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-            SwingUtilities.updateComponentTreeUI(frame);
+            SwingUtilities.updateComponentTreeUI(this);
             ForumClient fc = new ForumClient();
             UpdateListener ul = new UpdateListener(this, listenerPort);
             Thread t = new Thread(ul);
@@ -58,12 +70,12 @@ public class ClientMainGUI extends JFrame implements ActionListener {
         final int FRAME_WIDTH = 500;
         final int FRAME_HEIGHT = 600;
 
-        frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
-        frame.setTitle("Socket Forum");
+        this.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+        this.setTitle("Socket Forum");
         
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new BorderLayout());
-        frame.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new BorderLayout());
+        this.setLocationRelativeTo(null);
 
         JLabel Title = new JLabel("Socket Forum. User: " + user);
         Title.setFont(new Font("Serif", Font.BOLD, 30));
@@ -73,7 +85,7 @@ public class ClientMainGUI extends JFrame implements ActionListener {
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
         panel.setAutoscrolls(true);
         panel.setBackground(Color.GREEN);
-        frame.add(panel, BorderLayout.NORTH);
+        this.add(panel, BorderLayout.NORTH);
 
         JScrollPane scrollPane = new JScrollPane(panel);
         scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
@@ -113,8 +125,9 @@ public class ClientMainGUI extends JFrame implements ActionListener {
         if (posts != null){
 
             total_btns = posts.size();
-            comments = new List[total_btns];
-            btnComments = new JButton[posts.size()];
+           
+            comments = new ArrayList<List>(); //comments = new List[total_btns];
+           
             for (int i = 0; i < posts.size(); i++) {
                 Post p1 = (Post) posts.get(i);
                 JPanel sp1 = new JPanel(); //Post
@@ -122,13 +135,13 @@ public class ClientMainGUI extends JFrame implements ActionListener {
                 sp1.setBackground(Color.blue);
                 sp1.setPreferredSize(new Dimension(550, 200));
 
-                JPanel ssp1 = new JPanel(); //Cuadros y labels
+                ssp1 = new JPanel(); //Cuadros y labels
                 //ssp1.setLayout(new FlowLayout());
                 ssp1.setBackground(new Color(51, 153, 255));
                 ssp1.setPreferredSize(new Dimension(100, 200));
                 
 
-                JPanel ssp2 = new JPanel(); //Botones
+                ssp2 = new JPanel(); //Botones
                 ssp2.setLayout(new FlowLayout());
                 ssp2.setBackground(Color.WHITE);
                 ssp2.setPreferredSize(new Dimension(350, 170));
@@ -136,69 +149,42 @@ public class ClientMainGUI extends JFrame implements ActionListener {
                 ssp2.setBackground(new Color(51, 153, 255));
                 ssp2.setPreferredSize(new Dimension(300, 200));
 
-                JLabel pTitle = new JLabel(p1.getTitle());
-                pTitle.setForeground(Color.BLACK);
-                pTitle.setFont(new Font("Times New Roman", Font.BOLD, 12));
-                pTitle.setPreferredSize(new Dimension(100, 20));
+                createComponent(p1);
+                Post pactual = (Post)posts.get(i);
+                comments.add(pactual.getComments());
 
-                JLabel pTopic = new JLabel(p1.getTopic());
-                pTopic.setForeground(Color.BLACK);
-                pTopic.setFont(new Font("Times New Roman", Font.BOLD, 12));
-                pTopic.setPreferredSize(new Dimension(100, 20));
+                
+                ssp1.add(titlesLabels.get(i));
+                ssp1.add(topicsLabels.get(i));
+                ssp1.add(usersLabel.get(i));
+                ssp1.add(messagesLabel.get(i));
+                
+                ssp2.add(picsLabel.get(i));
+                ssp2.add(buttonsComment.get(i));
 
-                JLabel pUser = new JLabel(p1.getUser());
-                pUser.setForeground(Color.BLACK);
-                pUser.setFont(new Font("Times New Roman", Font.BOLD, 12));
-                pUser.setPreferredSize(new Dimension(100, 20));
-
-                JLabel pMessage = new JLabel(p1.getMessage());
-                pMessage.setForeground(Color.BLACK);
-                pMessage.setFont(new Font("Times New Roman", Font.BOLD, 12));
-                pMessage.setPreferredSize(new Dimension(100, 20));
-
-                /*JLabel pDate = new JLabel(p1.getDate());
-            pDate.setForeground(Color.BLACK);
-            pDate.setFont(new Font("Times New Roman", Font.BOLD, 12));
-            pDate.setPreferredSize(new Dimension(100, 20));*/
-                btnComments[i] = new JButton("Comments ");
-                comments[i] = ((Post)posts.get(i)).getComments();
-                btnComments[i].setPreferredSize(new Dimension(100, 20));
-                btnComments[i].addActionListener(this);
-                ssp1.add(pTitle);
-                ssp1.add(pTopic);
-                ssp1.add(pUser);
-                ssp1.add(pMessage);
-                //ssp1.add(pDate);
-                try {
-                    myPicture = ImageIO.read(new File(p1.getPath_img()));
-                } catch (IOException ex) {
-                    Logger.getLogger(ClientMainGUI.class.getName()).log(Level.SEVERE, null, ex);
-                }
-                picLabel = new JLabel(new ImageIcon(new ImageIcon(myPicture).getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT)));
-                ssp2.add(picLabel);
-                ssp2.add(btnComments[i]);
-
-                sp1.add(ssp1); //Cuadros y labels
-                sp1.add(ssp2); //Botones
+                sp1.add(ssp1); 
+                sp1.add(ssp2);
                 panel.add(sp1);
-
             }
         }
-        frame.add(panelDerecho, BorderLayout.EAST);
-        frame.add(Title, BorderLayout.NORTH);
-        frame.add(contentPane, BorderLayout.CENTER);
+        this.add(panelDerecho, BorderLayout.EAST);
+        this.add(Title, BorderLayout.NORTH);
+        this.add(contentPane, BorderLayout.CENTER);
         
-        frame.pack();
-        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        frame.setVisible(true);
+        this.pack();
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        this.setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == botonDe1) { // New Post
-            CreatePostGUI cpg = new CreatePostGUI(user, this);
-            this.setVisible(false);
-            cpg.setVisible(true);
+            CreatePostGUI cpg = new CreatePostGUI(current_user, this);
+            //this.setVisible(false);
+            this.dispose();
+            cpg.setVisible(true);   
+            this.repaint();
+            
         } else if (e.getSource() == botonDe2) { //Log Out
             Login l = new Login();
             l.setVisible(true);
@@ -214,7 +200,7 @@ public class ClientMainGUI extends JFrame implements ActionListener {
                 if (foundPosts != null){
                     for (int i = 0; i < foundPosts.size(); i++) {
                         Post p = (Post) foundPosts.get(i);
-                        System.out.println(p.toString());
+                        //System.out.println(p.toString());
                     }
                     // Usar los posts encontrado para mostrarlos
                 } else {
@@ -223,18 +209,18 @@ public class ClientMainGUI extends JFrame implements ActionListener {
             }
         }
         for(int i = 0; i < total_btns; i++) {
-            if(e.getSource() == btnComments[i]) {
+            if(e.getSource() == buttonsComment.get(i)) {
                 ForumDAO fd = new ForumDAO();
                 try {
-                    comments[i] = fd.getComments((Post)(posts.get(i)));
+                    comments.set(i, fd.getComments((Post)(posts.get(i))));
                 } catch (SQLException ex) {
                     Logger.getLogger(ClientMainGUI.class.getName()).log(Level.SEVERE, null, ex);
                 }
                 
                 Post post = (Post)posts.get(i);
-                System.out.println(post.toString());
+                //System.out.println(post.toString());
                 
-                new CommentGUI(comments[i], post.getIdpost(), current_user).setVisible(true);
+                new CommentGUI(comments.get(i), post.getIdpost(), current_user).setVisible(true);
             }
         }
         
@@ -248,7 +234,7 @@ public class ClientMainGUI extends JFrame implements ActionListener {
             allPosts = l;
             for (int i = 0; i < l.size(); i++) {
                 Post p = (Post) l.get(i);
-                System.out.println("\n" + p.toString());
+                //System.out.println("\n" + p.toString());
                 if (postWithImage.contains(p.getIdpost())){
                     continue;
                 }
@@ -276,5 +262,47 @@ public class ClientMainGUI extends JFrame implements ActionListener {
             }
         }
         return foundPosts;
+    }
+   
+    public void createComponent(Post p1) {
+        JLabel pTitle = new JLabel(p1.getTitle());
+        pTitle.setForeground(Color.BLACK);
+        pTitle.setFont(new Font("Times New Roman", Font.BOLD, 12));
+        pTitle.setPreferredSize(new Dimension(100, 20));
+        titlesLabels.add(pTitle);
+
+        JLabel pTopic = new JLabel(p1.getTopic());
+        pTopic.setForeground(Color.BLACK);
+        pTopic.setFont(new Font("Times New Roman", Font.BOLD, 12));
+        pTopic.setPreferredSize(new Dimension(100, 20));
+        topicsLabels.add(pTopic);
+        
+        JLabel pUser = new JLabel(p1.getUser());
+        pUser.setForeground(Color.BLACK);
+        pUser.setFont(new Font("Times New Roman", Font.BOLD, 12));
+        pUser.setPreferredSize(new Dimension(100, 20));
+        usersLabel.add(pUser);
+
+        JLabel pMessage = new JLabel(p1.getMessage());
+        pMessage.setForeground(Color.BLACK);
+        pMessage.setFont(new Font("Times New Roman", Font.BOLD, 12));
+        pMessage.setPreferredSize(new Dimension(100, 20));
+        messagesLabel.add(pMessage);
+        
+        try {
+            myPicture = ImageIO.read(new File(p1.getPath_img()));
+        } catch (IOException ex) {
+            Logger.getLogger(ClientMainGUI.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        picLabel = new JLabel(new ImageIcon(new ImageIcon(myPicture).getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT)));
+        picsLabel.add(picLabel);
+        
+        JButton btnactual = new JButton("Comments ");
+        btnactual.setPreferredSize(new Dimension(100, 20));
+        btnactual.addActionListener(this);
+        buttonsComment.add(btnactual);
+        
+        //ssp1.repaint();
+        //ssp2.repaint();
     }
 }
